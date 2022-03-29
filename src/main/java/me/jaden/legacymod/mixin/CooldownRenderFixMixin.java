60ae43fdacccf5dac32ffba6c900b61ee428d0dc
ilovefuud <jaden.johnson@hawkeyecollege.edu>
2022-03-29 04:41:28 -0500

    Update to 1.18.2 and remove swing fix

--- src/main/java/me/jaden/legacymod/mixin/CooldownRenderFixMixin.java
+++ src/main/java/me/jaden/legacymod/mixin/CooldownRenderFixMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,8 +16,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(HeldItemRenderer.class)
 public class CooldownRenderFixMixin {
 
+	@Final
 	@Shadow
-	MinecraftClient client;
+	private MinecraftClient client;
 
 	@ModifyVariable(method = "updateHeldItems()V", at = @At("STORE"), ordinal = 0)
 	private float injected(float x) {
